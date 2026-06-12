ad1688dd77a14b83219ac9f4a886a1299f833159
MDGCAT <michael@meetcarrot.xyz>
2026-06-11 21:44:43 -0700

    Zsh Makeover

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,31 +1,30 @@
-#!/bin/bash
-cd "$(dirname "${BASH_SOURCE}")"
+#!/usr/bin/env zsh
+cd "${0:A:h}"
 
-function doIt() {
-    exclude_list=".DS_store bootstrap.sh .functions .git .gitignore .gitmodules README.md setup.sh"
+doIt() {
+    exclude_list=".DS_Store bootstrap.sh .functions .git .gitignore .gitmodules README.md .vim .vimrc setup.sh"
 
     for file in .*; do
-        if ! [[ $exclude_list =~ $file ]]
-        then
+        if ! [[ $exclude_list =~ $file ]]; then
             # Check that we're not symlinking the same file. Untested, I'll punt it for now.
             #[ $(ls -la ~/  | grep $file | grep -o '\-> [^$]*' | awk '{print $2}') -eq $(pwd)/$file ]
-            if [ -d ~/$file ]; then # ln is weird for directories.
-                ln -s -i $(pwd)/$file ~
+            if [[ -d ~/$file ]]; then
+                ln -s -i "$(pwd)/$file" ~
             else
-                ln -s -i $(pwd)/$file ~/$file
+                ln -s -i "$(pwd)/$file" ~/$file
             fi
         fi
     done
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [[ "$1" == "--force" || "$1" == "-f" ]]; then
     doIt
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    read -q "REPLY?This may overwrite existing files in your home directory. Are you sure? (y/n) "
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         doIt
     fi
 fi
 unset doIt
-source ~/.bash_profile
+source ~/.zshrc
